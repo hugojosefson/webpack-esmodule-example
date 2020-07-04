d2d4fc678939d0c42a83ed75ee0beea3b6942bc2
Hugo Josefson <hugo@josefson.org>
2020-07-04 12:43:32 +0200

    Add alt attribute to img.

---
+++ src/common/image.jsx
@@ -0,0 +1,2 @@
+export default ({src, width = 300, height = 200, title}) =>
+<img src={src} width={width} height={height} title={title} alt={title}/>
--- src/image.jsx
+++
@@ -1,2 +0,0 @@
-export default ({src, width = 300, height = 200, title}) =>
-<img src={src} width={width} height={height} title={title}/>
