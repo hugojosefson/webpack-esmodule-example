2de5a38b344abb2a743221cc51cd38702d6421aa
Hugo Josefson <hugo@josefson.org>
2020-07-12 20:35:15 +0200

    Add link to repo.

--- src/common/app.jsx
+++ src/common/app.jsx
@@ -17,6 +17,7 @@ export default async () => {
       <p>If you are reading this on a <em>modern</em> web browser, it automatically uses modern JavaScript which is about 10x smaller and faster to load.</p>
       <p>If you are reading this on a <em>legacy</em> web browser, it automatically falls back to older JavaScript, transpiled down from the original, to make sure it still works everywhere.</p>
       <p>The benefit is that you can program for modern browsers and have them use the modern code. You don't have to transpile down everything to the lowest common denominator. Modern browsers get the fastest code!</p>
+      <p>See <a href="https://github.com/hugojosefson/webpack-esmodule-example">github.com/hugojosefson/webpack-esmodule-example</a> for source code.</p>
       <Greeting whom="corporate ipsum" type="h2"/>
       <p><i>poem from <a href="https://www.cipsum.com/">cipsum.com</a>.</i></p>
       <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
