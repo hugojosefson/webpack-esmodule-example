0db768ed43aed3e76c85a1a7150258ef2c96d3ee
Hugo Josefson <hugo@josefson.org>
2020-07-12 18:32:44 +0000

    Work around newline whitespace removal in text nodes.

--- src/common/app.jsx
+++ src/common/app.jsx
@@ -16,12 +16,8 @@ export default async () => {
       <p>Welcome. This is a short paragraph; others are not.</p>
       <Greeting whom="corporate ipsum" type="h2"/>
       <p><i>poem from <a href="https://www.cipsum.com/">cipsum.com</a>.</i></p>
-      {/*<p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to*/}
-      {/*  corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the*/}
-      {/*  holistic world view of disruptive innovation via workplace diversity and empowerment.</p>*/}
-      <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going
-        forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud
-        solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+      <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+      <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
       <div>{state.count}</div>
     </main>
 
