a5e34073876f3a3360d6abfc6466c10d8d9cf0c0
Hugo Josefson <hugo@josefson.org>
2022-01-16 14:43:17 +0100

    Clean up.

--- webpack/webpack.config.prod.cjs
+++
@@ -1,9 +0,0 @@
-/**
- * Wrapper hack until webpack properly supports config as ES Module.
- * Delete this file, and point any references to the .mjs file after
- * https://github.com/webpack/webpack-cli/issues/1622 is closed.
- */
-module.exports = async (env, argv) => {
-  const config = await import('./webpack.config.prod.mjs')
-  return config.default(env, argv)
-}
